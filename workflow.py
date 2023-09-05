45345977c095314c2ae75df8eecf7b160bb6c98f
SarthakBhardwajCode <85218961+sarthakbhardwaj27@users.noreply.github.com>
2023-09-05 16:07:23 +0530

    Add files via upload
    csv to test converted

---
+++ workflow.py
@@ -0,0 +1,19 @@
+frontend:
+1. dropdown to choose starting and ending station
+2. choose faster route or cheaper route
+
+output:
+1. stations list
+2. total cost
+3. total time taken
+
+backend:
+1. depending on prefernce of time or money, we will choose a path finding algorithm
+2. 
+
+dataset:
+1. https://www.kaggle.com/datasets/kunalgupta2616/delhi-metro-stations-data
+
+languages:
+html,css,js
+
